b29ee8fcd1248159b2cf9c3e03f35627d92fbebb
f01yy <79443616+f01yy@users.noreply.github.com>
2022-11-23 20:41:28 +0300

    separation of logic

--- js/clientFormHandling.js
+++ js/clientFormHandling.js
@@ -2,31 +2,26 @@ const totalBtn = document.querySelector('.total__btn');
 
 const clientForm = document.querySelector('.client__form');
 
-const nameInput = document.querySelector('.client__name');
-const surnameInput = document.querySelector('.client__surname');
-const emailInput = document.querySelector('.client__email');
-const phoneInput = document.querySelector('.client__number');
-const indexInput = document.querySelector('.client__index');
+const nameInput = document.querySelector('.client__name .client__input');
+const surnameInput = document.querySelector('.client__surname .client__input');
+const emailInput = document.querySelector('.client__email .client__input');
+const phoneInput = document.querySelector('.client__number .client__input');
+const indexInput = document.querySelector('.client__index .client__input');
 
 totalBtn.addEventListener('click', () => {
   if (!nameInput.value.length) {
-    console.log('name');
     nameInput.classList.add('empty-input');
   }
   if (!surnameInput.value.length) {
-    console.log('surname');
     surnameInput.classList.add('empty-input');
   }
   if (!emailInput.value.length) {
-    console.log('email');
     emailInput.classList.add('empty-input');
   }
   if (!phoneInput.value.length) {
-    console.log('phone');
     phoneInput.classList.add('empty-input');
   }
   if (!indexInput.value.length) {
-    console.log('index');
     indexInput.classList.add('empty-input');
   }
 });
@@ -38,9 +33,8 @@ clientForm.addEventListener('input', (e) => {
   } else if (e.target === surnameInput) {
   } else if (e.target === emailInput) {
     if (validateEmail(e.target.value) && e.target.value.length) {
-      console.log('ok');
       emailInput.classList.remove('wrong-input');
-      document.querySelector('.email-warn')?.remove();
+      document.querySelector('.client__email .warn')?.remove();
     }
   } else if (e.target === phoneInput) {
     const ALLOWED_PHONE_SYMBOLS = '0123456789+';
@@ -61,70 +55,60 @@ clientForm.addEventListener('input', (e) => {
       e.target.value.slice(-2);
 
     if (validatePhone(e.target.value) && e.target.value.length) {
-      console.log('ok');
       phoneInput.classList.remove('wrong-input');
-      document.querySelector('.phone-warn')?.remove();
+      document.querySelector('.client__number .warn')?.remove();
     }
   } else if (e.target === indexInput) {
     if (validateIndex(e.target.value) && e.target.value.length) {
       const indexWarn = document.querySelector('.index-info');
-      console.log('ok');
       indexInput.classList.remove('wrong-input');
-      indexWarn.classList.remove('index-warn');
+      indexWarn.classList.remove('warn');
       indexWarn.innerHTML = 'Для таможенного оформления';
     }
   }
 });
 
 clientForm.addEventListener('focusout', (e) => {
-  console.log(e.target);
-
   if (e.target === nameInput) {
   } else if (e.target === surnameInput) {
   } else if (e.target === emailInput) {
     if (!validateEmail(e.target.value) && e.target.value.length) {
-      if (!document.querySelector('.email-warn')) {
+      if (!document.querySelector('.client__email .warn')) {
         const emailWarn = document.createElement('div');
-        console.log('not ok');
         emailWarn.innerHTML = 'Проверьте адрес электронной почты';
-        emailWarn.className = 'email-warn';
+        emailWarn.className = 'warn';
         emailInput.after(emailWarn);
         emailInput.classList.add('wrong-input');
       }
     } else if (validateEmail(e.target.value) && e.target.value.length) {
-      console.log('ok');
       emailInput.classList.remove('wrong-input');
-      document.querySelector('.email-warn')?.remove();
+      document.querySelector('.client__email .warn')?.remove();
     }
   } else if (e.target === phoneInput) {
     if (!validatePhone(e.target.value) && e.target.value.length) {
-      if (!document.querySelector('.phone-warn')) {
+      if (!document.querySelector('.client__number .warn')) {
         const phoneWarn = document.createElement('div');
-        console.log('not ok');
         phoneWarn.innerHTML = 'Формат: +9 999 999 99 99 или 9 999 999 99 99';
-        phoneWarn.className = 'phone-warn';
+        phoneWarn.className = 'warn';
         phoneInput.after(phoneWarn);
         phoneInput.classList.add('wrong-input');
       }
     } else if (validatePhone(e.target.value) && e.target.value.length) {
-      console.log('ok');
       phoneInput.classList.remove('wrong-input');
-      document.querySelector('.phone-warn')?.remove();
+      document.querySelector('.client__number .warn')?.remove();
     }
   } else if (e.target === indexInput) {
     if (!validateIndex(e.target.value) && e.target.value.length) {
       const indexWarn = document.querySelector('.index-info');
 
-      if (!document.querySelector('.index-warn')) {
-        console.log('not ok');
+      if (!document.querySelector('.client__index .warn')) {
         indexWarn.innerHTML = 'Формат: 1234567';
-        indexWarn.classList.add('index-warn');
+        indexWarn.classList.add('warn');
         indexInput.classList.add('wrong-input');
       }
     } else if (validateIndex(e.target.value) && e.target.value.length) {
-      console.log('ok');
       indexInput.classList.remove('wrong-input');
-      indexWarn.classList.remove('index-warn');
+      indexWarn.classList.remove('warn');
     }
   }
 });
@@ -149,3 +133,36 @@ function validatePhone(value) {
 
   return PHONE_REGEXP.test(value);
 }
+
+clientForm.addEventListener('focusin', (e) => {
+  if (e.target === nameInput) {
+    const nameHint = document.createElement('div');
+    nameHint.innerHTML = 'Укажите имя';
+    nameHint.className = 'hint';
+    nameInput.after(nameHint);
+  } else if (e.target === surnameInput) {
+    const surnameHint = document.createElement('div');
+    surnameHint.innerHTML = 'Введите фамилию';
+    surnameHint.className = 'hint';
+    surnameInput.after(surnameHint);
+  } else if (e.target === emailInput) {
+    const emailHint = document.createElement('div');
+    emailHint.innerHTML = 'Укажите email';
+    emailHint.className = 'hint';
+    emailInput.after(emailHint);
+  } else if (e.target === phoneInput) {
+    const phoneHint = document.createElement('div');
+    phoneHint.innerHTML = 'Укажите номер телефона';
+    phoneHint.className = 'hint';
+    phoneInput.after(phoneHint);
+  } else if (e.target === indexInput) {
+    const indexHint = document.createElement('div');
+    indexHint.innerHTML = 'Укажите индекс';
+    indexHint.className = 'hint';
+    indexInput.after(indexHint);
+  }
+});
+
+clientForm.addEventListener('focusout', (e) => {
+  document.querySelector(`.${e.target.parentNode.className} .hint`).remove();
+});
